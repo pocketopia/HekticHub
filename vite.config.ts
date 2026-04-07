f79961ca8c273a579ff7b3010d1a8f23abee2e63
pocketopia <orchestraofdeath@gmail.com>
2026-04-07 12:37:58 -0700

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,9 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
+// https://vitejs.dev/config/
 export default defineConfig({
-  base: '/HekticHub/',
+  // We changed this from '/HekticHub/' to '/' because you are using a custom domain
+  base: '/', 
   plugins: [react()],
 })
